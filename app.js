d6261a58294954943d2e7b1d1f0222270067bbc8
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-18 12:28:05 +0530

    Making my name print on the localhost 4000 server.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-//generally keep the name app.js can also keep server.js
+const http = require('http');
 
-//importing core modules/ files
-const http = require('http'); //'./' looks for a local file if omitting this it looks for global files
+// Replace 'Your Name' with your actual name
+const yourName = 'Somya';
 
-//the functions in http files accessed as below
-const server = http.createServer((req, res) => { //called by nodeJs whenever there is a request
-    console.log(req);
-});  //it looks for the given function to response to the responses per request
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-server.listen(4000) //created server has been store in const called server this listener doesnt act immideately bt listens to the incomming requests
+  // Log your name to the console
+  console.log(yourName);
+
+  // Send your name as the response
+  res.end(yourName);
+});
+
+// Listen on port 4000
+const port = 4000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
