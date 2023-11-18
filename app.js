e776a07136aa9c3e95079941af81173ff01febb9
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-18 12:56:14 +0530

    Understanding to send responses.

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-const http = require('http');
+//generally keep the name app.js can also keep server.js
 
-// Replace 'Your Name' with your actual name
-const yourName = 'Somya';
+//importing core modules/ files
+const http = require('http'); //'./' looks for a local file if omitting this it looks for global files
 
-const server = http.createServer((req, res) => {
-  // Set the response header
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+//the functions in http files accessed as below
+const server = http.createServer((req, res) => { //called by nodeJs whenever there is a request
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');  //using this function we assign the header type
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body>Hello this is my HTML code.</body>");
+    res.write('<html>'); //it actually assigns the values that the programmer puts
+    res.end();
+});  //it looks for the given function to response to the responses per request
 
-  // Log your name to the console
-  console.log(yourName);
-
-  // Send your name as the response
-  res.end(yourName);
-});
-
-// Listen on port 4000
-const port = 4000;
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+server.listen(4000) //created server has been store in const called server this listener doesnt act immideately bt listens to the incomming requests
