94bf5a34822bcb63e372580867067b7ceb8fc287
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-18 13:06:20 +0530

    Adding various pages and displaying the welcome messages to that paths in url.

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-//generally keep the name app.js can also keep server.js
+const http = require('http');
 
-//importing core modules/ files
-const http = require('http'); //'./' looks for a local file if omitting this it looks for global files
-
-//the functions in http files accessed as below
-const server = http.createServer((req, res) => { //called by nodeJs whenever there is a request
+const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');  //using this function we assign the header type
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body>Hello this is my HTML code.</body>");
-    res.write('<html>'); //it actually assigns the values that the programmer puts
-    res.end();
-});  //it looks for the given function to response to the responses per request
+    res.setHeader('Content-Type', 'text/html');
 
-server.listen(4000) //created server has been store in const called server this listener doesnt act immideately bt listens to the incomming requests
+    if (req.url === '/home') {
+        res.write("<html><head><title>Home Page</title></head><body>Welcome home</body></html>");
+    } else if (req.url === '/about') {
+        res.write("<html><head><title>About Us Page</title></head><body>Welcome to About Us page</body></html>");
+    } else if (req.url === '/node') {
+        res.write("<html><head><title>Node.js Project</title></head><body>Welcome to my Node Js project</body></html>");
+    } else {
+        res.write("<html><head><title>Not Found</title></head><body>404 - Not Found</body></html>");
+    }
+
+    res.end();
+});
+
+server.listen(4000);
