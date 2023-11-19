dab43426f2d565d7b415769e0b8c275865b4ce28
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-19 11:33:32 +0530

    Using stream and buffer in node.Js.

--- app.js
+++ app.js
@@ -16,7 +16,18 @@ const server = http.createServer((req, res) => {
 
     //for saving the message in the post request
     if(url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) => { //it recieves a chunk of data
+            console.log(chunk);
+            body.push(chunk);
+        }); //on method allows to listen to certain events; data event is fired whenever a new chunk of data is there
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        
         res.statusCode = 302; //if there is any error then the user gets redirected to this 302 error page
         res.setHeader('Location', '/'); //automatically redirects to the host being used
         return res.end();
