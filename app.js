e13fbd977a288bb9790aece2dc5d1d12a4bdf632
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-19 12:18:10 +0530

    Using the node module system and making new filke called routes.js and connecting it with the app.js.

--- app.js
+++ app.js
@@ -1,61 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-     
-    const url = req.url;
-    const method = req.method;
-    
-    if (url === '/') {
-        // Read existing messages from the file
-        const existingMessages = fs.readFileSync('message.txt', 'utf8').split('\n').filter(Boolean);
+const routes = require('./routes'); //it is a local file not global
 
-        res.write('<html>');
-        res.write("<head><title>Enter Message</title></head>");
-        res.write("<body>");
-        
-        // Display existing messages at the top of the form
-        if (existingMessages.length > 0) {
-            res.write("<h2>Existing Messages:</h2>");
-            res.write("<ul>");
-            existingMessages.forEach(message => {
-                res.write(`<li>${message}</li>`);
-            });
-            res.write("</ul>");
-        }
-
-        res.write("<form action='/message' method='POST'><label>Enter your message:- </label><input type='text' name='message'><button>Send</button></form>");
-        res.write("</body>");
-        res.write("</html>");
-        return res.end();
-    }
-
-    // For saving the message in the post request
-    if(url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            
-            // Append the new message to the file
-            fs.appendFileSync('message.txt', message + '\n');
-        });
-        
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my node.js server!</h1></body>')
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(4000);
---
+++ routes.js
@@ -0,0 +1,34 @@
+// For saving the message in the post request
+const fs = require('fs');
+
+// Function to handle requests
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        // Read existing messages from the file
+        const existingMessages = fs.readFileSync('message.txt', 'utf8').split('\n').filter(Boolean);
+    
+        res.write('<html>');
+        res.write("<head><title>Enter Message</title></head>");
+        res.write("<body>");
+        
+        // Display existing messages at the top of the form
+        if (existingMessages.length > 0) {
+            res.write("<h2>Existing Messages:</h2>");
+            res.write("<ul>");
+            existingMessages.forEach(message => {
+                res.write(`<li>${message}</li>`);
+            });
+            res.write("</ul>");
+        }
+    
+        res.write("<form action='/message' method='POST'><label>Enter your message:- </label><input type='text' name='message'><button>Send</button></form>");
+        res.write("</body>");
+        res.write("</html>");
+        return res.end();
+    }
+}
+
+module.exports = requestHandler; // Export the function
