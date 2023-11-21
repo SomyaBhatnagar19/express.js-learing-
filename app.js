8596877afd5cc18e4356f97a870f3ea2f014d173
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-21 11:37:28 +0530

    Understanding to handle different routes using use().

--- app.js
+++ app.js
@@ -3,18 +3,14 @@ const express = require('express');
 const app = express();
 
 //middleware functions
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); //the request wont be able to continue its journey to the next middleware if we dont call next
-    //allows the request to continue to next middleware in line
-}); //use helps assign middleware functions, the function passed in use is working for ever request
-//next is a fucntion passed as argument abovewhich gets passed to other middleware function
-
-//middleware2
-app.use((req, res, next) => {
+//using use for routess
+app.use('/add-product', (req, res, next) => {
+    console.log('In 2nd middleware!');
+    res.send("<h1>The add product page.</h1>");
+});
+app.use('/', (req, res, next) => {
     console.log('In 2nd middleware!');
     res.send("<h1>Hello from Express.js!</h1>");
-    // instead of using res.write we use send, this method automatically sets the content as text/html
 });
 
 
