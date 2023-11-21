f12382dbae5827c3f420b85b529e1eedffcb4f21
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-21 11:56:05 +0530

    Parsing incoming request using package called body-parser.

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser'); //importing body-parser
 
 const app = express();
 
+//below use is for parsing the request of body as it is undefined right now
+app.use(bodyParser.urlencoded({extended: false})); //registers a middleware 
+
+
 //middleware functions
-//using use for routess
+//using use for routess- if /app-product is used as path then 
+        //only below middleware works else the below one works
 app.use('/add-product', (req, res, next) => {
-    console.log('In 2nd middleware!');
-    res.send("<h1>The add product page.</h1>");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
+
+//middleware to handle /product 
+app.use('/product', (req, res, next) => {
+    console.log(req.body); 
+    res.redirect('/'); //this helps to redirect to some other page
+});
+
+
 app.use('/', (req, res, next) => {
-    console.log('In 2nd middleware!');
     res.send("<h1>Hello from Express.js!</h1>");
 });
 
