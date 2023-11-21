b7cbb1ec294c8ab1e2ab03c2ee96f4d29a01f290
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-21 11:15:48 +0530

    Creating middleware functions after installing express and importing it, using use function and send fucntions.

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
-const routes = require('./routes'); //it is a local file not global
-console.log(routes.someText);
-// const server = http.createServer(routes);
-const server = http.createServer(routes.handler);
+const express = require('express');
+
+const app = express();
+
+//middleware functions
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); //the request wont be able to continue its journey to the next middleware if we dont call next
+    //allows the request to continue to next middleware in line
+}); //use helps assign middleware functions, the function passed in use is working for ever request
+//next is a fucntion passed as argument abovewhich gets passed to other middleware function
+
+//middleware2
+app.use((req, res, next) => {
+    console.log('In 2nd middleware!');
+    res.send(<h1>Hello from Express.js!</h1>);
+    // instead of using res.write we use send, this method automatically sets the content as text/html
+});
+const server = http.createServer(app);
+
 server.listen(4000);
