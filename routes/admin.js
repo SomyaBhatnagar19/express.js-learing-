e5a7c1098ebda939391ade01b21fadf9fe007f28
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-21 13:06:32 +0530

    Filtering paths for both admin and shop so that we can see the data.

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><label>Product name: </label><input type="text" name="title"><label>Size of Product: </label><input type="number" name="size"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><label>Product name: </label><input type="text" name="title"><label>Size of Product: </label><input type="number" name="size"><button type="submit">Add Product</button></form>');
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     const title = req.body.title;
     const size = req.body.size;
